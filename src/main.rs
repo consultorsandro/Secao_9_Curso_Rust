0f1d579900904e19df8ac8343a561f442c9765a4
Sandro Reis <consultorsandro@hotmail.com>
2025-05-16 08:25:57 -0300

    Class 147

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 fn main() {
-    //Class 146
+    	//Class 147
+	struct Coffee {
+		price: f64,
+		name: String,
+		is_hot: bool,
+		};
+		
+let mut beverage = Coffee {
+	name: String::from("mocha"),
+	price: 4.99,
+	is_hot: false,
+	};
+	
+	beverage.name = String::from("Caramel Macchiato");
+	beverage.price = 6.99;
+	beverage.is_hot = true;
+	
+	println!(
+	"My {} this morning cost {}. It is {} that it was hot", 
+		beverage.name, beverage.price, beverage.is_hot
+		);
+	
+}
+    /*//Class 146
     struct Coffee {
         price: f64,
         name: String,
@@ -11,7 +34,7 @@ fn main() {
         price: 4.99,
         is_hot: false,
     };
-}
+}*/
 /* Class 145
 println!(
     "My {} this morning cost {}. It is {} that it was hot",
